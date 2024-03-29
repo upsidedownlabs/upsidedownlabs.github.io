08a5cc6467dae0d98a8bb75f300e3df932eae9aa
Deepak Khatri <deepak@upsidedownlabs.tech>
2024-03-29 13:39:35 +0530

    WIP: find error cause

--- conf.py
+++ conf.py
@@ -20,14 +20,15 @@ copyright = '2024, Upside Down Labs'
 author = 'Upside Down Labs'
 
 # PDF path exploration
-
-MAX_DEPTH = 1
+MAX_DEPTH = 2
 walk_dirpaths = ['hardware/bioamp']
 pdf_paths = []
 for walk_dirpath in walk_dirpaths:
     for (dirpath, dirnames, filenames) in os.walk(walk_dirpath, topdown=True):
-        if dirpath.count('/') == 2:
-            pdf_paths.append(dirpath)
+        if dirpath.count('/') == MAX_DEPTH:
+            pdf_paths.append(dirpath[:])
+
+print(pdf_paths)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
