4fb01a7c883b48e9e09567f61608fc78c353750e
Bhawna Sehgal <bhawnasehgal.1999@gmail.com>
2024-04-13 18:48:08 +0530

    Update DIY Neuroscience Kit basic content

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ author = 'Upside Down Labs'
 
 # PDF path exploration
 MAX_DEPTH = 2
-walk_dirpaths = ['hardware','kits']
+walk_dirpaths = ['hardware', 'kits']
 pdf_paths = []
 for walk_dirpath in walk_dirpaths:
     for (dirpath, dirnames, filenames) in os.walk(walk_dirpath, topdown=True):
